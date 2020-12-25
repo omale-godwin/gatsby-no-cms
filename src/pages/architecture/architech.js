1ea48cf9122fbf411089d618e30fb26dd66792c3
omale-godwin <godwinpeace3@gmail.com>
2020-12-25 03:12:45 +0000

    solve window not define issue by using get url path npm

--- src/pages/architecture/architech.js
+++ src/pages/architecture/architech.js
@@ -11,7 +11,7 @@ const Architecture = ({ data }) => {
 
   function singlepage(e) {
     console.log(e);
-    navigate(`/architecture/post?id=${e}`, 
+    navigate(`/architecture/post?name=${e}`, 
     {
       state: {numbers: e}
     })
