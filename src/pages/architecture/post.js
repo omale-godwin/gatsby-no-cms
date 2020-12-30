92deabb5f8ffd58c573748b8f0e97fbcbe8d25ed
omale-godwin <godwinpeace3@gmail.com>
2020-12-30 04:45:22 +0000

    componrntdidmouttest

--- src/pages/architecture/post.js
+++ src/pages/architecture/post.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import parser from 'html-react-parser';
 import Layout from '../../components/Layout';
 import data from '../../categories/architectures.json'
 
-function post() {
-  const search = window.location.search;
-  const params = new URLSearchParams(search);
-  const id = params.get('id');
+export class Post extends Component {
+  constructor (){
+    super()
+    this.state = {
+    postid: ''
+    }
+  };
+
+  componentDidMount(){
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    this.setState({postid: params.get('id')});
+  
+  }
+  render(){
+    if(this.state.postid == null || this.state.postid == undefined || this.state.postid == ''){
+      return(
+        <div>
+          <h1>loading</h1>
+        </div>
+      )
+    }else{
 
   return (
     <div>
        <Layout>
       <div className="container">
-          <h1>{data[id - 1].title}</h1>
+          <h1>{data[this.state.postid - 1].title}</h1>
           <br /><br />
-          {parser(data[id - 1].content.replace(/\n/gi, "<br />"))};
+          {parser(data[this.state.postid - 1].content.replace(/\n/gi, "<br />"))};
       
           </div>
         </Layout>
     </div>
-  )
+  )}}
 }
 
-export default post
+export default Post
